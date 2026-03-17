654a30669b7a66d53fdec92e670b5f74b90afa4d
FASTRATABUANA\dian.budianto <dian.budianto@fastratabuana.co.id>
2026-03-17 13:39:43 +0700

    implement dotenv & migration

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import server from './server/index.js';
  
-const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+const host = process.env.HOST !== 'production' ? 'localhost' : '0.0.0.0';
 const port = process.env.PORT || 3000;
  
 server.listen(port, () => {
- console.log(`Server running at http://${host}:${port}`);
+    console.log(`Server running at http://${host}:${port}`);
 });
