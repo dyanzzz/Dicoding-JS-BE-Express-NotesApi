2f9ba660c93b16d29a592c08b5e42ee44b1ded84
FASTRATABUANA\dian.budianto <dian.budianto@fastratabuana.co.id>
2026-03-28 15:19:41 +0700

    impement upload images with multer

--- src/server/index.js
+++ src/server/index.js
@@ -8,5 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler);
+app.use('/uploads', express.static('src/services/uploads/files/images'));
 
 export default app;
